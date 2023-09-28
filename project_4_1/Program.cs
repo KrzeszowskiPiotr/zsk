e22b76675445904acb785a6432120f3fec8af1b4
KrzeszowskiPiotr <144985806+KrzeszowskiPiotr@users.noreply.github.com>
2023-09-28 08:40:17 +0200

    Add files via upload

---
+++ project_4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "imIę I nAzwiskO";
+            Console.WriteLine(message);
+            
+
+            string messageUppercase = message.ToUpper();
+            Console.WriteLine(messageUppercase);
+
+            string messageLowercase = message.ToLower();
+            Console.WriteLine(messageLowercase);
+
+            string name = message.Substring(1);
+            Console.WriteLine(name);
+
+            string name1 = message.Substring(0, 4);
+            Console.WriteLine(name1);
+
+            Console.WriteLine(name1[0]);
+
+            Console.WriteLine(message.Substring(0,1).ToUpper() + message.Substring(1).ToLower());
+            Console.Write(message[0].ToString().ToUpper() + message.Substring(1).ToLower();
+        }
+    }
+}
