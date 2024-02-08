c4cd8cfcff40d9cdb200d4708b5b2c6cec631391
KrzeszowskiPiotr <144985806+KrzeszowskiPiotr@users.noreply.github.com>
2024-02-08 08:41:04 +0100

    Add files via upload

---
+++ ConsoleApp1/Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    internal class Car
+    {
+        public string Brand { get;private set; }
+        public string Model { get;private set; }
+        public ushort Year { get;private set; }
+        public Color Color { get;private set; }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"To jest {Brand} {Model} z {Year}, kolor: {Color}");
+        }
+    }
+}
